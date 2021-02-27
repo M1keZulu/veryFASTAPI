a06f37f21cee0081412f8b553e1e7ce35926dd8b
Muhammad Zain Ul Abiddin <afsarhussain9@hotmail.com>
2021-02-28 02:00:55 +0500

    Effeciency Update - Filing

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from flask_restful import reqparse
 import re
 from openpyxl import load_workbook
 import urllib
+import json
 
 app = Flask(__name__)
 
@@ -41,17 +42,9 @@ def details(course):
     course = urllib.parse.unquote_plus(course)
     wb = load_workbook(filename='timetable.xlsx')
     l = []
-    for sheet in wb.worksheets:
-        if sheet.title=="Monday" or sheet.title=="Tuesday" or sheet.title=="Wednesday" or sheet.title=="Thursday" or sheet.title=="Friday" or sheet.title=="Saturday" or sheet.title=="Sunday":
-            for i in range(1, sheet.max_row + 1):
-                for j in range(sheet.max_column):
-                    if (str(sheet[i][j].value).find("BCS") != -1 or str(sheet[i][j].value).find("BDS") != -1 or str(sheet[i][j].value).find("BAI") != -1 or str(sheet[i][j].value).find("BSE") != -1):
-                        l.append({"Course Name": (" ".join(sheet[i][j].value.split()).upper().replace("/", "&")), "Day": (sheet.title.upper()), "Venue: ": (sheet[i][0].value.upper()), "Slot": (sheet[2][j].value), "Timings": time_setter((sheet[2][j].value))})
-                        if str(sheet[i][j].value).find("Lab") != -1 or str(sheet[i][j].value).find("lab") != -1:
-                            l.append({"Course Name": (" ".join(sheet[i][j].value.split()).upper().replace("/", "&")), "Day": (sheet.title.upper()), "Venue: ": (sheet[i][0].value.upper()), "Slot": (sheet[2][j].value)+1, "Timings": time_setter((sheet[2][j].value)+1)})
-                            l.append({"Course Name": (" ".join(sheet[i][j].value.split()).upper().replace("/", "&")), "Day": (sheet.title.upper()), "Venue: ": (sheet[i][0].value.upper()), "Slot": (sheet[2][j].value)+2, "Timings": time_setter((sheet[2][j].value)+2)})
+    with open("data.txt", "r") as f:
+        l = json.load(f)
 
-    
     r = []
     for x in l:
         if x["Course Name"] == course:
@@ -78,15 +71,9 @@ def search():
         s.append(section)
 
     l = []
-    for sheet in wb.worksheets:
-        if sheet.title=="Monday" or sheet.title=="Tuesday" or sheet.title=="Wednesday" or sheet.title=="Thursday" or sheet.title=="Friday" or sheet.title=="Saturday" or sheet.title=="Sunday":
-            for i in range(1, sheet.max_row + 1):
-                for j in range(sheet.max_column):
-                    if (str(sheet[i][j].value).find("BCS") != -1 or str(sheet[i][j].value).find("BDS") != -1 or str(sheet[i][j].value).find("BAI") != -1 or str(sheet[i][j].value).find("BSE") != -1):
-                        l.append({"Course Name": (" ".join(sheet[i][j].value.split()).upper().replace("/", "&")), "Day": (sheet.title.upper()), "Venue: ": (sheet[i][0].value.upper()), "Slot": (sheet[2][j].value), "Timings": time_setter((sheet[2][j].value))})
-                        if str(sheet[i][j].value).find("Lab") != -1 or str(sheet[i][j].value).find("lab") != -1:
-                            l.append({"Course Name": (" ".join(sheet[i][j].value.split()).upper().replace("/", "&")), "Day": (sheet.title.upper()), "Venue: ": (sheet[i][0].value.upper()), "Slot": (sheet[2][j].value)+1, "Timings": time_setter((sheet[2][j].value)+1)})
-                            l.append({"Course Name": (" ".join(sheet[i][j].value.split()).upper().replace("/", "&")), "Day": (sheet.title.upper()), "Venue: ": (sheet[i][0].value.upper()), "Slot": (sheet[2][j].value)+2, "Timings": time_setter((sheet[2][j].value)+2)})
+    with open("data.txt", "r") as f:
+        l = json.load(f)
+
     r = []
     for x in l:
         print(s)
@@ -103,14 +90,9 @@ def search():
 
 @app.route('/names')
 def names():
-    wb = load_workbook(filename='timetable.xlsx')
-    l = []
-    for sheet in wb.worksheets:
-        if sheet.title=="Monday" or sheet.title=="Tuesday" or sheet.title=="Wednesday" or sheet.title=="Thursday" or sheet.title=="Friday" or sheet.title=="Saturday" or sheet.title=="Sunday":
-            for i in range(1, sheet.max_row + 1):
-                for j in range(sheet.max_column):
-                    if str(sheet[i][j].value).find("BCS") != -1 or str(sheet[i][j].value).find("BDS") != -1 or str(sheet[i][j].value).find("BAI") != -1 or str(sheet[i][j].value).find("BSE") != -1:
-                        l.append({"Course Name": (" ".join(sheet[i][j].value.split()).upper().replace("/", "&")), "Day": (sheet.title.upper()), "Venue: ": (sheet[i][0].value.upper()), "Slot": (sheet[2][j].value)})
+    with open("data.txt", "r") as f:
+        l = json.load(f)
+        
     r = []
     for x in l:
         if x["Course Name"] not in r:
@@ -119,16 +101,8 @@ def names():
 
 @app.route('/navigate')
 def nav():
-    wb = load_workbook(filename='timetable.xlsx')
-    l = []
-    dupli=[]
-    for sheet in wb.worksheets:
-        if sheet.title=="Monday" or sheet.title=="Tuesday" or sheet.title=="Wednesday" or sheet.title=="Thursday" or sheet.title=="Friday" or sheet.title=="Saturday" or sheet.title=="Sunday":
-            for i in range(1, sheet.max_row + 1):
-                for j in range(sheet.max_column):
-                    if (str(sheet[i][j].value).find("BCS") != -1 or str(sheet[i][j].value).find("BDS") != -1 or str(sheet[i][j].value).find("BAI") != -1 or str(sheet[i][j].value).find("BSE") != -1) and (" ".join(sheet[i][j].value.split()).upper().replace("/", "&")) not in dupli:
-                        l.append({"Course Name": (" ".join(sheet[i][j].value.split()).upper().replace("/", "&")), "Day": (sheet.title.upper()), "Venue: ": (sheet[i][0].value.upper()), "Slot": (sheet[2][j].value)})
-
+    with open("data.txt", "r") as f:
+        l = json.load(f)
     course_names = [[]]
 
     for x in l:
@@ -136,5 +110,3 @@ def nav():
             course_names.append([x["Course Name"], "details/" + urllib.parse.quote_plus(x["Course Name"])])
     
     return render_template("index.html", course_names=course_names)
-
-
