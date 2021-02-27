66c23fe49a86c9975490f2d4deca458913474c5d
Muhammad Zain Ul Abiddin <afsarhussain9@hotmail.com>
2021-02-27 20:39:41 +0500

    Added Timings

--- app.py
+++ app.py
@@ -5,6 +5,27 @@ import urllib
 
 app = Flask(__name__)
 
+def time_setter(t):
+    if(t==1):
+        return "8:00-9:00"
+    elif(t==2):
+        return "9:00-10:00"
+    elif(t==3):
+        return "10:00-11:00"
+    elif(t==4):
+        return "11:00-12:00"
+    elif(t==5):
+        return "12:00-1:00"
+    elif(t==6):
+        return "1:00-2:00"
+    elif(t==7):
+        return "2:00-3:00"
+    elif(t==8):
+        return "3:00-4:00"
+    elif(t==9):
+        return "4:00-5:00"
+    
+
 @app.route('/details/<string:course>')
 def details(course):
     course = urllib.parse.unquote_plus(course)
@@ -15,10 +36,10 @@ def details(course):
             for i in range(1, sheet.max_row + 1):
                 for j in range(sheet.max_column):
                     if (str(sheet[i][j].value).find("BCS") != -1 or str(sheet[i][j].value).find("BDS") != -1 or str(sheet[i][j].value).find("BAI") != -1 or str(sheet[i][j].value).find("BSE") != -1):
-                        l.append({"Course Name": (" ".join(sheet[i][j].value.split()).upper().replace("/", "&")), "Day": (sheet.title.upper()), "Venue: ": (sheet[i][0].value.upper()), "Slot": (sheet[2][j].value)})
+                        l.append({"Course Name": (" ".join(sheet[i][j].value.split()).upper().replace("/", "&")), "Day": (sheet.title.upper()), "Venue: ": (sheet[i][0].value.upper()), "Slot": (sheet[2][j].value), "Timings": time_setter((sheet[2][j].value))})
                         if str(sheet[i][j].value).find("Lab") != -1 or str(sheet[i][j].value).find("lab") != -1:
-                            l.append({"Course Name": (" ".join(sheet[i][j].value.split()).upper().replace("/", "&")), "Day": (sheet.title.upper()), "Venue: ": (sheet[i][0].value.upper()), "Slot": (sheet[2][j].value)+1})
-                            l.append({"Course Name": (" ".join(sheet[i][j].value.split()).upper().replace("/", "&")), "Day": (sheet.title.upper()), "Venue: ": (sheet[i][0].value.upper()), "Slot": (sheet[2][j].value)+2})
+                            l.append({"Course Name": (" ".join(sheet[i][j].value.split()).upper().replace("/", "&")), "Day": (sheet.title.upper()), "Venue: ": (sheet[i][0].value.upper()), "Slot": (sheet[2][j].value)+1, "Timings": time_setter((sheet[2][j].value)+1)})
+                            l.append({"Course Name": (" ".join(sheet[i][j].value.split()).upper().replace("/", "&")), "Day": (sheet.title.upper()), "Venue: ": (sheet[i][0].value.upper()), "Slot": (sheet[2][j].value)+2, "Timings": time_setter((sheet[2][j].value)+2)})
 
     
     r = []
@@ -38,10 +59,6 @@ def names():
                 for j in range(sheet.max_column):
                     if str(sheet[i][j].value).find("BCS") != -1 or str(sheet[i][j].value).find("BDS") != -1 or str(sheet[i][j].value).find("BAI") != -1 or str(sheet[i][j].value).find("BSE") != -1:
                         l.append({"Course Name": (" ".join(sheet[i][j].value.split()).upper().replace("/", "&")), "Day": (sheet.title.upper()), "Venue: ": (sheet[i][0].value.upper()), "Slot": (sheet[2][j].value)})
-                        if str(sheet[i][j].value).find("Lab") != -1 or str(sheet[i][j].value).find("lab") != -1:
-                            l.append({"Course Name": (" ".join(sheet[i][j].value.split()).upper().replace("/", "&")), "Day": (sheet.title.upper()), "Venue: ": (sheet[i][0].value.upper()), "Slot": (sheet[2][j].value)+1})
-                            l.append({"Course Name": (" ".join(sheet[i][j].value.split()).upper().replace("/", "&")), "Day": (sheet.title.upper()), "Venue: ": (sheet[i][0].value.upper()), "Slot": (sheet[2][j].value)+2})
-    
     r = []
     for x in l:
         if x["Course Name"] not in r:
