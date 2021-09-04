72c13bc7dc4648c0b30bb39932bae0012f828fb1
Muhammad Zain Ul Abiddin <68997634+M1keZulu@users.noreply.github.com>
2021-09-04 18:49:31 +0500

    Old System Reinitiated

--- app.py
+++ app.py
@@ -3,11 +3,8 @@ from flask_restful import reqparse
 import re
 from openpyxl import load_workbook
 import urllib
-import json
 
 app = Flask(__name__)
-if __name__ == '__main__':
-    app.run(threaded=True)
 
 parser = reqparse.RequestParser()
 parser.add_argument('day', type=str, required=False)
@@ -38,14 +35,21 @@ def time_setter(t):
     elif(t==9):
         return "4:00-5:00"
     
-
 @app.route('/details/<string:course>')
 def details(course):
     course = urllib.parse.unquote_plus(course)
+    wb = load_workbook(filename='timetable.xlsx')
     l = []
-    with open("data.txt", "r") as f:
-        l = json.load(f)
-
+    for sheet in wb.worksheets:
+        if sheet.title=="Monday" or sheet.title=="Tuesday" or sheet.title=="Wednesday" or sheet.title=="Thursday" or sheet.title=="Friday" or sheet.title=="Saturday" or sheet.title=="Sunday":
+            for i in range(1, sheet.max_row + 1):
+                for j in range(sheet.max_column):
+                    if (str(sheet[i][j].value).find("BCS") != -1 or str(sheet[i][j].value).find("BDS") != -1 or str(sheet[i][j].value).find("BAI") != -1 or str(sheet[i][j].value).find("BSE") != -1):
+                        l.append({"Course Name": (" ".join(sheet[i][j].value.split()).upper().replace("/", "&")), "Day": (sheet.title.upper()), "Venue: ": (sheet[i][0].value.upper()), "Slot": (sheet[2][j].value), "Timings": time_setter((sheet[2][j].value))})
+                        if str(sheet[i][j].value).find("Lab") != -1 or str(sheet[i][j].value).find("lab") != -1:
+                            l.append({"Course Name": (" ".join(sheet[i][j].value.split()).upper().replace("/", "&")), "Day": (sheet.title.upper()), "Venue: ": (sheet[i][0].value.upper()), "Slot": (sheet[2][j].value)+1, "Timings": time_setter((sheet[2][j].value)+1)})
+                            l.append({"Course Name": (" ".join(sheet[i][j].value.split()).upper().replace("/", "&")), "Day": (sheet.title.upper()), "Venue: ": (sheet[i][0].value.upper()), "Slot": (sheet[2][j].value)+2, "Timings": time_setter((sheet[2][j].value)+2)})
+    
     r = []
     for x in l:
         if x["Course Name"] == course:
@@ -53,13 +57,14 @@ def details(course):
     return jsonify(r)
 @app.route('/search')
 def search():
+    wb = load_workbook(filename='timetable.xlsx')
     program = str(parser.parse_args().get('program', None)).upper()
     section = str(parser.parse_args().get('section', None)).upper()
     day = str(parser.parse_args().get('day', None)).upper()
     teacher = str(parser.parse_args().get('teacher', None)).upper()
     slot = parser.parse_args().get('slot', None)
     course = str(parser.parse_args().get('course', None)).upper()
-    
+
     s=[]
     if program!="NONE":
         s.append(program)
@@ -71,9 +76,15 @@ def search():
         s.append(section)
 
     l = []
-    with open("data.txt", "r") as f:
-        l = json.load(f)
-
+    for sheet in wb.worksheets:
+        if sheet.title=="Monday" or sheet.title=="Tuesday" or sheet.title=="Wednesday" or sheet.title=="Thursday" or sheet.title=="Friday" or sheet.title=="Saturday" or sheet.title=="Sunday":
+            for i in range(1, sheet.max_row + 1):
+                for j in range(sheet.max_column):
+                    if (str(sheet[i][j].value).find("BCS") != -1 or str(sheet[i][j].value).find("BDS") != -1 or str(sheet[i][j].value).find("BAI") != -1 or str(sheet[i][j].value).find("BSE") != -1):
+                        l.append({"Course Name": (" ".join(sheet[i][j].value.split()).upper().replace("/", "&")), "Day": (sheet.title.upper()), "Venue: ": (sheet[i][0].value.upper()), "Slot": (sheet[2][j].value), "Timings": time_setter((sheet[2][j].value))})
+                        if str(sheet[i][j].value).find("Lab") != -1 or str(sheet[i][j].value).find("lab") != -1:
+                            l.append({"Course Name": (" ".join(sheet[i][j].value.split()).upper().replace("/", "&")), "Day": (sheet.title.upper()), "Venue: ": (sheet[i][0].value.upper()), "Slot": (sheet[2][j].value)+1, "Timings": time_setter((sheet[2][j].value)+1)})
+                            l.append({"Course Name": (" ".join(sheet[i][j].value.split()).upper().replace("/", "&")), "Day": (sheet.title.upper()), "Venue: ": (sheet[i][0].value.upper()), "Slot": (sheet[2][j].value)+2, "Timings": time_setter((sheet[2][j].value)+2)})
     r = []
     for x in l:
         print(s)
@@ -85,26 +96,36 @@ def search():
             continue
         if all(y in x["Course Name"] for y in s):
             r.append(x)
-        
+
     return jsonify(r)
 
 @app.route('/names')
 def names():
-    with open("data.txt", "r") as f:
-        l = json.load(f)
-        
+    wb = load_workbook(filename='timetable.xlsx')
+    l = []
+    for sheet in wb.worksheets:
+        if sheet.title=="Monday" or sheet.title=="Tuesday" or sheet.title=="Wednesday" or sheet.title=="Thursday" or sheet.title=="Friday" or sheet.title=="Saturday" or sheet.title=="Sunday":
+            for i in range(1, sheet.max_row + 1):
+                for j in range(sheet.max_column):
+                    if str(sheet[i][j].value).find("BCS") != -1 or str(sheet[i][j].value).find("BDS") != -1 or str(sheet[i][j].value).find("BAI") != -1 or str(sheet[i][j].value).find("BSE") != -1:
+                        l.append({"Course Name": (" ".join(sheet[i][j].value.split()).upper().replace("/", "&")), "Day": (sheet.title.upper()), "Venue: ": (sheet[i][0].value.upper()), "Slot": (sheet[2][j].value)})
     r = []
     for x in l:
         if x["Course Name"] not in r:
             r.append(x["Course Name"])
     return jsonify(r)
-
 @app.route('/navigate')
 def nav():
-    with open("data.txt", "r") as f:
-        l = json.load(f)
+    wb = load_workbook(filename='timetable.xlsx')
+    l = []
+    dupli=[]
+    for sheet in wb.worksheets:
+        if sheet.title=="Monday" or sheet.title=="Tuesday" or sheet.title=="Wednesday" or sheet.title=="Thursday" or sheet.title=="Friday" or sheet.title=="Saturday" or sheet.title=="Sunday":
+            for i in range(1, sheet.max_row + 1):
+                for j in range(sheet.max_column):
+                    if (str(sheet[i][j].value).find("BCS") != -1 or str(sheet[i][j].value).find("BDS") != -1 or str(sheet[i][j].value).find("BAI") != -1 or str(sheet[i][j].value).find("BSE") != -1) and (" ".join(sheet[i][j].value.split()).upper().replace("/", "&")) not in dupli:
+                        l.append({"Course Name": (" ".join(sheet[i][j].value.split()).upper().replace("/", "&")), "Day": (sheet.title.upper()), "Venue: ": (sheet[i][0].value.upper()), "Slot": (sheet[2][j].value)})
     course_names = [[]]
-
     for x in l:
         if x["Course Name"] not in course_names:
             course_names.append([x["Course Name"], "details/" + urllib.parse.quote_plus(x["Course Name"])])
